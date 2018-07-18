89162c132311fb51ecf9a3ba8d2b2808a9268d2a
baoyonghui <baoyonghui@139.com>
2018-07-18 19:11:05 +0800

    这是参照例子做的摄像头控制

---
+++ DJICameraDemo/app/src/main/java/cn/com/mcfly/djicamerademo/MApplication.java
@@ -0,0 +1,23 @@
+package cn.com.mcfly.djicamerademo;
+
+import android.app.Application;
+import android.content.Context;
+import com.secneo.sdk.Helper;
+
+public class MApplication extends Application {
+    private FPVDemoApplication fpvDemoApplication;
+    @Override
+    protected void attachBaseContext(Context paramContext) {
+        super.attachBaseContext(paramContext);
+        Helper.install(MApplication.this);
+        if (fpvDemoApplication == null) {
+            fpvDemoApplication = new FPVDemoApplication();
+            fpvDemoApplication.setContext(this);
+        }
+    }
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        fpvDemoApplication.onCreate();
+    }
+}
